7c373f215562468af582c890189e2d36f0129ff5
trevorfitz0 <trevorfitz0@gmail.com>
2023-04-19 11:10:14 -0600

    Artist Names now showing up on background image

---
+++ src/Components/Result/Result.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import './Result.css'
+import beach from '../../Images/beach.png'
+import mountain from '../../Images/mountains.png'
+import night from '../../Images/night.png'
+
+function Result({ background, artistList }) {
+
+    console.log(artistList)
+
+    const backgroundImage = {
+        beach,
+        mountain,
+        night
+      }[background];
+
+      var acc = 0
+    
+
+  return (
+    <div>
+        <div className='result-background'>
+            <div className='main-area'>
+                <h1 className='title'>Ravify!</h1>
+                <h1>You're gonna have a good time!</h1>
+            </div>
+            <p className='line-break'></p>
+            <div className='display-area'>
+                <img className='final-poster' alt='final poster' src={ backgroundImage }></img>
+                <div className='artists'>
+                    <h1 className='headliner'>{artistList[0].name}</h1>
+                {
+                    artistList.map(item => {
+                        acc++
+                        if (acc === 1) {
+                            return
+                        } else if (acc <= 3) {
+                            return <h2 className={`direct-${acc}`} >{item.name}</h2>
+                        } else {
+                            return <h3>{item.name}</h3>
+                        }
+                    })
+                }
+                </div>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Result
