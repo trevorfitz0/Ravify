6e00ef122b1d716cc23fbe988f51503d5f0b46bc
Trevor Fitzgerald <57536985+trevorfitz0@users.noreply.github.com>
2024-03-17 23:16:24 -0600

    Save image
    Fix for saving maybe

--- src/Components/Result/Result.js
+++ src/Components/Result/Result.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Result.css';
 import beach from '../../Images/beach.png';
 import mountain from '../../Images/mountains.png';
@@ -7,82 +7,81 @@ import city from '../../Images/city.png';
 import nightForest from '../../Images/night-forest.png';
 import { Link } from 'react-router-dom';
 import Header from '../Header/Header';
-import { toPng } from 'html-to-image'; // Updated import statement
+import { toPng } from 'html-to-image';
 
 function Result({ background, artistList, loading, logOut }) {
 
-    function captureAndDownload() {
-        const posterElement = document.querySelector('.poster');
-      
-        toPng(posterElement) // Updated usage of toPng
-          .then(function (dataUrl) {
-            // Create a temporary anchor element
-            const downloadLink = document.createElement('a');
-            downloadLink.href = dataUrl;
-            downloadLink.download = 'poster.png';
-      
-            // Trigger the download
-            document.body.appendChild(downloadLink);
-            downloadLink.click();
-      
-            // Clean up
-            document.body.removeChild(downloadLink);
-          })
-          .catch(function (error) {
-            console.error('Error capturing image:', error);
-          });
-      }
+    function downloadImage() {
+        // Select the poster element
+        const poster = document.querySelector('.ss');
 
-  const backgroundImage = {
-    beach,
-    mountain,
-    night,
-    city,
-    nightForest
-  }[background];
+        // Convert the poster to PNG image
+        toPng(poster)
+            .then(function (dataUrl) {
+                // Create a temporary link element
+                const link = document.createElement('a');
+                link.href = dataUrl;
+                link.download = 'poster.png';
 
-  var acc = 0;
+                // Trigger a click event on the link to start the download
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            })
+            .catch(function (error) {
+                console.error('Error generating PNG image:', error);
+            });
+    }
 
-  if (loading) {
-    return null;
-  } else {
-    return (
-      <div>
-        <div className='home-background'>
-          <Header logOut={logOut} />
-          <div className='poster'>
-            <img className='final-poster' alt='final poster' src={backgroundImage}></img>
-            <h1 className='festival-name'>Ravify.me</h1>
-            <p className="linebreak"></p>
-            <div className='artists'>
-              <h1 className='headliner'>{artistList[0].name}</h1>
-              <br />
-              <div className='direct-support'>
-                <h2 className={`direct-1`}>{artistList[1].name}</h2>
-                <h2 className={`direct-2`}>{artistList[2].name}</h2>
-              </div>
+    const backgroundImage = {
+        beach,
+        mountain,
+        night,
+        city,
+        nightForest
+    }[background];
+
+    var acc = 0;
+
+    if (loading) {
+        return null;
+    } else {
+        return (
+            <div>
+                <div className='home-background'>
+                    <Header logOut={logOut} />
+                    <div className='ss'>
+                    <div className='poster'>
+                            <img className='final-poster' alt='final poster' src={backgroundImage}></img>
+                            <div className='artists'>
+                                <h1 className='headliner'>{artistList[0].name}</h1>
+                                <br />
+                                <div className='direct-support'>
+                                    <h2 className={`direct-1`}>{artistList[1].name}</h2>
+                                    <h2 className={`direct-2`}>{artistList[2].name}</h2>
+                                </div>
+                            </div>
+                            <div className='all-artists'>
+                                {artistList.map(item => {
+                                    acc++;
+                                    if (acc > 3) {
+                                        return <h3 key={item.name}>{item.name}</h3>;
+                                    }
+                                })}
+                                <i className="fa-brands fa-spotify fa-xl" id='spotify-logo' size="2xl" style={{ color: "#ffffff" }}></i>
+                            </div>
+                        </div>
+                    </div>
+                    <Link to='/background' className='change-background-button'>Change Background</Link>
+                    <div className='share-section'>
+                        <div className='html2can'>
+                            <button onClick={downloadImage}>Download Image</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className='all-artists'>
-              {artistList.map(item => {
-                acc++;
-                if (acc > 3) {
-                  return <h3 key={item.name}>{item.name}</h3>;
-                }
-              })}
-              <i className="fa-brands fa-spotify fa-xl" id='spotify-logo' size="2xl" style={{ color: "#ffffff" }}></i>
-              
-            </div>
-          </div>
-          <Link to='/background' className='change-background-button'>Change Background</Link>
-          <div className='share-section'>
-            <div className='html2can'>
-              <h4 onClick={() => captureAndDownload()}>Download image!</h4>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default Result;
